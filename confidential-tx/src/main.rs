ea7810a79cd04b1a7b021f307e523431d9af9d1a
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-02-16 11:35:54 +0000

    feat: working POC

--- confidential-tx/src/main.rs
+++ confidential-tx/src/main.rs
@@ -12,6 +12,8 @@ fn main() {
     let G_bt = EdwardsBasepointTable::create(&G);
     let H = G * &Scalar::from(scalar2);
     let H_bt = EdwardsBasepointTable::create(&H);
+    let J = H * &Scalar::from(scalar2);
+    let J_bt = EdwardsBasepointTable::create(&J);
 
     // println!("Random point on the curve: {:?}", H);
 
@@ -22,29 +24,34 @@ fn main() {
     let vH_1 = EdwardsBasepointTable::basepoint_mul(&H_bt, &v1);
 
     //TxIN2 from the article example (a=30, r=85): 85*G, 30*H
-    let r2 = Scalar::from(85u64);
-    let v2 = Scalar::from(30u64);
+    let r2 = Scalar::from(14u64);
+    let v2 = Scalar::from(10u64);
+    let t2 = Scalar::from(20u64);
+    let test = Scalar::from(0u64);
     let rG_2 = EdwardsBasepointTable::basepoint_mul(&G_bt, &r2);
-    let vH_2 = EdwardsBasepointTable::basepoint_mul(&H_bt, &v2);
+    let rG_4 = EdwardsBasepointTable::basepoint_mul(&H_bt, &v2);
+    let vH_2 = EdwardsBasepointTable::basepoint_mul(&J_bt, &t2);
+    let zero = EdwardsBasepointTable::basepoint_mul(&H_bt, &test);
+    let zero2 = EdwardsBasepointTable::basepoint_mul(&G_bt, &test);
 
     // //TxIN3 from the article example (a=10, r=43): 43*G, 10*H
     let r3 = Scalar::from(43u64);
     let v3 = Scalar::from(10u64);
-    let rG_3 = EdwardsBasepointTable::basepoint_mul(&G_bt, &r3);
+    let rG_3 = EdwardsBasepointTable::basepoint_mul(&H_bt, &r3);
     let vH_3 = EdwardsBasepointTable::basepoint_mul(&H_bt, &v3);
 
     //calculate pedersen EC commitments for the TxIN's
     //TxIN1: c=14*G+10*H
-    let c1 = rG_1 + vH_1;
+    let c1 = rG_1 + vH_1; // G(14) + H(10)
     //TxIN2: c=85*G+30*H
-    let c2 = rG_2 + vH_2;
+    let c2 = rG_2 + vH_2 + rG_4; // G(14) + H(10) + J(20)
     //TxIN3: c=43*G+10*H
-    let c3 = rG_3 + vH_3;
+    let c3 = zero + zero2 + vH_2; // G(0) + H(0) + J(20);
 
     let combined = rG_1 + vH_1 + rG_2 + vH_2 + rG_3 + vH_3;
     println!(
         "Do properties hold: {:?}",
-        (c1 + c2 + c3).compress() == combined.compress()
+        (c2 - c1).compress() == c3.compress()
     );
 
     //blinding factor and curve point for TxOUT1 from the article example (a=40, r=28): 28*G, 40*H
@@ -77,14 +84,14 @@ fn main() {
     //TxOUT3, a.k.a the TxOUT for the fee, (a=2, r=83): 83*G, 2*H
     //the 83 is calculated in the following as the diff between input and output G
     let r_i_total = r1 + r2 + r3;
-    let r_o_total = r4 + r5;
+    let r_o_total = r4 + r5; 
     let r_fee = r_i_total - r_o_total; // 84*G
     let v_fee = Scalar::from(2u64); // 2*H
     let rG_fee = EdwardsBasepointTable::basepoint_mul(&G_bt, &r_fee);
     let vH_fee = EdwardsBasepointTable::basepoint_mul(&H_bt, &v_fee);
     let c_fee = rG_fee + vH_fee;
 
-    //combined commitment for all TxOUT's together
+    //combined commitment for all TxOUT's together 
     let c_all_o = c4 + c5 + c_fee;
 
     //calculate directly the commitment to zero, z=0*G + 0*H
