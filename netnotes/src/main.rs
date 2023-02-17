844a98a1322633414f11e7c52173b250c72dc1f0
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-02-17 16:27:11 +0000

    feat: blockchain and POW

--- netnotes/src/main.rs
+++ netnotes/src/main.rs
@@ -1,15 +1,38 @@
 mod block;
 use block::Block;
+mod blockchain;
+use blockchain::Blockchain;
 
 fn main() {
     let mut block = Block::new(
-        0,
         0,
         vec![0; 32],
         vec![0; 32],
         0,
         "Genesis Block".to_string(),
+        0x00fffffffffffffffffffffffffffff,
     );
-    block.block_hash = block.hash();
-    println!("{:?}", block);
+    block.mine();
+
+    let last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    let mut block2 = Block::new(
+        1,
+        vec![0; 32],
+        last_hash,
+        0,
+        "Test block".to_string(),
+        0x00ffffffffffffffffffffffffffffffff,
+    );
+    block2.mine();
+
+    blockchain.blocks.push(block2);
+
+    blockchain.verify();
+
+    println!("{:?}", blockchain);
 }
