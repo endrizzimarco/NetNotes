dd822a828ee6131f0bce222040a91f2e8cfa6118
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-04-01 21:14:04 +0200

    feat: mimblewimble benchmark

--- benches/mimblewimble_benchmark.rs
+++ benches/mimblewimble_benchmark.rs
@@ -1,10 +1,26 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use curve25519_dalek::scalar::Scalar;
 use netnotes::mimblewimble::{ResponseData, SendData, Transaction};
-use netnotes::pedersen;
+use netnotes::pedersen::{Commitment, GENS};
 use rand::rngs::OsRng;
 
-fn gen_mw_transaction(blinding_factors: Vec<Scalar>) -> Transaction {
+#[derive(Clone)]
+struct InputData {
+    amount: Scalar,
+    change: Scalar,
+    blinding_factors: Vec<Scalar>,
+    inputs: Vec<Commitment>,
+}
+
+fn inputs() -> Vec<InputData> {
+    let bf_small = gen_blinding_factors(2u32);
+    let bf_medium = gen_blinding_factors(2u32.pow(5));
+    let bf_large = gen_blinding_factors(2u32.pow(8));
+
+    vec![setup(bf_small), setup(bf_medium), setup(bf_large)]
+}
+
+fn setup(blinding_factors: Vec<Scalar>) -> InputData {
     // create a vector of random values same size of blinding_factors vector
     let values = blinding_factors
         .iter()
@@ -15,8 +31,8 @@ fn gen_mw_transaction(blinding_factors: Vec<Scalar>) -> Transaction {
     let inputs = values
         .iter()
         .zip(blinding_factors.iter())
-        .map(|(value, blinding_factor)| pedersen::commit(*value, *blinding_factor))
-        .collect::<Vec<pedersen::Commitment>>();
+        .map(|(value, blinding_factor)| GENS.commit(*value, *blinding_factor))
+        .collect::<Vec<Commitment>>();
 
     // pick change as 1
     let change = Scalar::one();
@@ -24,13 +40,26 @@ fn gen_mw_transaction(blinding_factors: Vec<Scalar>) -> Transaction {
     // sum values to an integer and subtract change
     let amount = values.iter().fold(Scalar::zero(), |acc, x| acc + x) - change;
 
-    // Simulate transaction
-    let tx_data = Transaction::init(amount, change, blinding_factors, inputs.clone());
-    let send_data = tx_data.send();
-    let response_data = SendData::respond(&send_data);
-    let transaction = ResponseData::finalise(&tx_data, &response_data);
+    InputData {
+        amount,
+        change,
+        blinding_factors,
+        inputs,
+    }
+}
 
-    // return
+fn gen_mw_transaction(input: InputData) -> Transaction {
+    // Simulate transaction
+    let tx_data = Transaction::init(
+        input.amount,
+        input.change,
+        input.blinding_factors,
+        input.inputs,
+    ); // 260 us
+    let send_data = tx_data.send(); // 261 us
+    let response_data = SendData::respond(&send_data); // 470 us
+    let transaction = ResponseData::finalise(&tx_data, &response_data); // 625 us
+
     transaction
 }
 
@@ -41,33 +70,26 @@ fn gen_blinding_factors(n: u32) -> Vec<Scalar> {
 }
 
 pub fn mimblewimble_tx_gen_benchmark(c: &mut Criterion) {
-    let bf_small = gen_blinding_factors(2u32.pow(10));
-    let bf_medium = gen_blinding_factors(2u32.pow(14));
-    let bf_large = gen_blinding_factors(2u32.pow(16));
+    let mut group = c.benchmark_group("mimblewimble_gen");
 
-    c.bench_function("mw_gen:2^10", |b| {
-        b.iter(|| gen_mw_transaction(black_box(bf_small.clone())))
-    });
-    c.bench_function("mw_gen:2^14", |b| {
-        b.iter(|| gen_mw_transaction(black_box(bf_medium.clone())))
-    });
-    c.bench_function("mw_gen:2^16", |b| {
-        b.iter(|| gen_mw_transaction(black_box(bf_large.clone())))
-    });
+    for i in inputs().iter() {
+        group.bench_with_input(format!("mw_gen:{}", i.blinding_factors.len()), i, |b, i| {
+            b.iter(|| gen_mw_transaction(black_box(i.clone())))
+        });
+    }
 }
 
 pub fn mimblewimble_tx_verify_benchmark(c: &mut Criterion) {
-    let bf_small = gen_blinding_factors(2u32.pow(10));
-    let bf_medium = gen_blinding_factors(2u32.pow(14));
-    let bf_large = gen_blinding_factors(2u32.pow(16));
+    let mut group = c.benchmark_group("mimblewimble_verify");
 
-    let tx_small = gen_mw_transaction(bf_small.clone());
-    let tx_medium = gen_mw_transaction(bf_medium.clone());
-    let tx_large = gen_mw_transaction(bf_large.clone());
-
-    c.bench_function("mw_verify:2^10", |b| b.iter(|| tx_small.verify()));
-    c.bench_function("mw_verify:2^14", |b| b.iter(|| tx_medium.verify()));
-    c.bench_function("mw_verify:2^16", |b| b.iter(|| tx_large.verify()));
+    for i in inputs().iter() {
+        let transaction = gen_mw_transaction(i.clone());
+        group.bench_with_input(
+            format!("mw_verify:{}", i.blinding_factors.len()),
+            i,
+            |b, i| b.iter(|| transaction.verify()),
+        );
+    }
 }
 
 criterion_group!(gen_benches, mimblewimble_tx_gen_benchmark);
