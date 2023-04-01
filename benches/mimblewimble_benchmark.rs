8972dd612d331a867b0faf47e67314a7087965eb
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-04-01 22:26:02 +0200

    refactor: complete mw benchmarks

--- benches/mimblewimble_benchmark.rs
+++ benches/mimblewimble_benchmark.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use curve25519_dalek::scalar::Scalar;
 use netnotes::mimblewimble::{ResponseData, SendData, Transaction};
 use netnotes::pedersen::{Commitment, GENS};
@@ -13,11 +13,12 @@ struct InputData {
 }
 
 fn inputs() -> Vec<InputData> {
-    let bf_small = gen_blinding_factors(2u32);
-    let bf_medium = gen_blinding_factors(2u32.pow(5));
-    let bf_large = gen_blinding_factors(2u32.pow(8));
+    let small = gen_blinding_factors(2u32.pow(5));
+    let medium = gen_blinding_factors(2u32.pow(10));
+    let large = gen_blinding_factors(2u32.pow(14));
+    let x_large = gen_blinding_factors(2u32.pow(16));
 
-    vec![setup(bf_small), setup(bf_medium), setup(bf_large)]
+    vec![setup(small), setup(medium), setup(large), setup(x_large)]
 }
 
 fn setup(blinding_factors: Vec<Scalar>) -> InputData {
@@ -69,29 +70,24 @@ fn gen_blinding_factors(n: u32) -> Vec<Scalar> {
         .collect::<Vec<Scalar>>()
 }
 
-pub fn mimblewimble_tx_gen_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("mimblewimble_gen");
+pub fn mimblewimble_benchmarks(c: &mut Criterion) {
+    let mut group = c.benchmark_group("mimblewimble");
 
     for i in inputs().iter() {
-        group.bench_with_input(format!("mw_gen:{}", i.blinding_factors.len()), i, |b, i| {
-            b.iter(|| gen_mw_transaction(black_box(i.clone())))
-        });
-    }
-}
-
-pub fn mimblewimble_tx_verify_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("mimblewimble_verify");
-
-    for i in inputs().iter() {
+        group.bench_with_input(
+            BenchmarkId::new("Generation", i.blinding_factors.len()),
+            i,
+            |b, i| b.iter(|| gen_mw_transaction(black_box(i.clone()))),
+        );
         let transaction = gen_mw_transaction(i.clone());
         group.bench_with_input(
-            format!("mw_verify:{}", i.blinding_factors.len()),
+            BenchmarkId::new("Verification", i.blinding_factors.len()),
             i,
-            |b, i| b.iter(|| transaction.verify()),
+            |b, _i| b.iter(|| transaction.verify()),
         );
     }
+    group.finish();
 }
 
-criterion_group!(gen_benches, mimblewimble_tx_gen_benchmark);
-criterion_group!(verify_benches, mimblewimble_tx_verify_benchmark);
-criterion_main!(gen_benches, verify_benches);
+criterion_group!(gen_benches, mimblewimble_benchmarks);
+criterion_main!(gen_benches);
